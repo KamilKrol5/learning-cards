8f678530877f1db6edf439b59e757dfb40cac709
Kamil <kamil55525@gmail.com>
2019-11-15 21:17:45 +0100

    Add models for LearningSet and Item

--- backend/LearningCards/models.py
+++ backend/LearningCards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class LearningSet(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    creation_date = models.DateField(auto_now_add=True)
+
+
+class Item(models.Model):
+    term = models.CharField(max_length=100, blank=False)
+    definition = models.TextField()
+    learning_set_id = models.ForeignKey(LearningSet, on_delete=models.CASCADE)

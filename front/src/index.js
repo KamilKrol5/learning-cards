d010f16f6b2f59d8715f0f61d4893f9b8e835726
dzazef <dzazef@gmail.com>
2019-11-16 00:33:34 +0100

    Add redux store with basic reducer and action for auth

--- front/src/index.js
+++ front/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import {store} from './store/store';
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+    ,
+
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

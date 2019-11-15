9b4539230764514c890f1a81b0528b2eac8ed1d0
Kamil <kamil55525@gmail.com>
2019-11-15 22:42:06 +0100

    Add api for searching learning set by name and api for retrieve/update/delete single item or learning set by primary key

--- backend/LearningCards/api/urls.py
+++ backend/LearningCards/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import LearningSetsListAPIView, ItemsListAPIView
+from .views import LearningSetsListAPIView, ItemsListAPIView, LearningSetRetrieveUpdateDestroyAPIView,\
+    ItemRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('sets/', LearningSetsListAPIView.as_view()),
-    # path('sets/<pk>', LearningSetsListAPIView.as_view()),
+    path('sets/<pk>', LearningSetRetrieveUpdateDestroyAPIView.as_view()),
     path('items/', ItemsListAPIView.as_view()),
-    # path('items/<pk>', ItemsListAPIView.as_view())
+    path('items/<pk>', ItemRetrieveUpdateDestroyAPIView.as_view())
 ]
--- backend/LearningCards/api/views.py
+++ backend/LearningCards/api/views.py
@@ -1,13 +1,29 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from LearningCards import models
 from . import serializers
 
 
 class LearningSetsListAPIView(ListCreateAPIView):
+    serializer_class = serializers.LearningSetSerializer
+
+    def get_queryset(self):
+        queryset = models.LearningSet.objects.all()
+        owner = self.request.user.id
+        name = self.request.query_params.get('search', '')
+        queryset = queryset.filter(owner=owner, name__contains=name)
+        return queryset
+
+
+class LearningSetRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = models.LearningSet.objects.all()
     serializer_class = serializers.LearningSetSerializer
 
 
 class ItemsListAPIView(ListCreateAPIView):
+    queryset = models.Item.objects.all()
+    serializer_class = serializers.ItemSerializer
+
+
+class ItemRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = models.Item.objects.all()
     serializer_class = serializers.ItemSerializer

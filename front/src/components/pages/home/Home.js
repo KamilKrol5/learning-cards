210c6c281dbbe4a4ead5f792e1ebe0386e797b0f
dzazef <dzazef@gmail.com>
2019-11-19 02:40:19 +0100

    Very small update

--- front/src/components/pages/home/Home.js
+++ front/src/components/pages/home/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
     render() {
         return (
             <div id="m-home-page">
-                <h1>Trzeba coś zrobić z tym tłem bo daje po oczach</h1>
+                <h1>Fiszki bla bla bla bla bla bla bla bla bla</h1>
                 <h1>cholera wie co</h1>
                 <h1>bla bla bla</h1>
                 <Link className="m-home-page-btn" to={"/signup"}>Begin</Link>

3400a971decbbd75dd32ee7ec09c23f971f58173
Kamil <kamil55525@gmail.com>
2019-11-22 00:42:16 +0100

    Add registering users to api functionality

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('LearningCards.api.urls'))
+    # path('api-auth/', include('rest_framework.urls')),
 ]

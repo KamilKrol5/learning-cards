af9b028bcfab05a348aaae0329f48851a54523f5
dzazef <dzazef@gmail.com>
2019-11-22 22:17:31 +0100

    Finish logging

--- front/src/components/pages/ApiTest.js
+++ front/src/components/pages/ApiTest.js
@@ -1,38 +1,38 @@
-import React, {Component} from 'react';
-import * as TestUserActions from "../../store/actions/testActions";
-import {connect} from "react-redux";
-
-/**
- * Komponent do testowania działania API
- */
-class ApiTest extends Component {
-    render() {
-        return (
-            <div>
-                <p>{JSON.stringify(this.props.randomUser)}</p>
-                <button onClick={e => {
-                    e.preventDefault();
-                    this.props.fetchUser();
-                }}>Click me
-                </button>
-            </div>
-        );
-    }
-}
-
-
-const mapStateToProps = state => {
-    return {
-        randomUser: state.randomUser,
-    }
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        fetchUser: () => {
-            dispatch(TestUserActions.fetchRandomUser())
-        }
-    }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ApiTest);
+// import React, {Component} from 'react';
+// import * as TestUserActions from "../../store/actions/testActions";
+// import {connect} from "react-redux";
+//
+// /**
+//  * Komponent do testowania działania API
+//  */
+// class ApiTest extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <p>{JSON.stringify(this.props.randomUser)}</p>
+//                 <button onClick={e => {
+//                     e.preventDefault();
+//                     this.props.fetchUser();
+//                 }}>Click me
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+//
+//
+// const mapStateToProps = state => {
+//     return {
+//         randomUser: state.randomUser,
+//     }
+// };
+//
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         fetchUser: () => {
+//             dispatch(TestUserActions.fetchRandomUser())
+//         }
+//     }
+// };
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(ApiTest);

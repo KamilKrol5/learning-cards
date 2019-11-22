a39232eea7c0d2a6703f15bd84c63877197fa89a
Kamil <kamil55525@gmail.com>
2019-11-22 23:09:57 +0100

    Add deploy.sh

---
+++ backend/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Connecting to lc@206.81.21.127 via ssh."
+ssh lc@206.81.21.127 'cd /home/lc/backend/learning-cards;
+git pull;
+if [[ '"$1"' == "run" ]]; then
+    cd /home/lc/backend/learning-cards/backend/venv/bin;
+    source activate; cd ../..;
+    python3 manage.py runserver 206.81.21.127:8000;
+    cd /home/lc/backend/learning-cards;
+    tail -f debug.log;
+fi;'

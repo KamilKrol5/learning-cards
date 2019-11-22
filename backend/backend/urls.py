9a5e2fb9cea60d1fa49237c50bb40a1a3e4ef2c1
Kamil <kamil55525@gmail.com>
2019-11-22 21:37:38 +0100

    Add token verify endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/', include('LearningCards.api.urls'))

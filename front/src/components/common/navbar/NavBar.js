351fbeb73b9d25796731650d61df60f9442ad011
dzazef <dzazef@gmail.com>
2019-11-27 02:35:18 +0100

    Begin learning mode #1

--- front/src/components/common/navbar/NavBar.js
+++ front/src/components/common/navbar/NavBar.js
@@ -15,9 +15,9 @@ class NavBar extends Component {
                 <div className="m-nav-right">
                     <Link className="m-nav-btn m-nav-link" to="/login">Login</Link>
                     <Link className="m-nav-btn m-nav-link" to="/signup">Register</Link>
-                    <Link className="m-nav-btn m-nav-link" to="/mode1">Mode 1 (in dev)</Link>
-                    <Link className="m-nav-btn m-nav-link" to="/flipcard">FlipCard (in dev)</Link>
-                    <Link className="m-nav-btn m-nav-link" to="/card">Card (in dev)</Link>
+                    <Link className="m-nav-btn m-nav-link" to="/mode1">Mode 1 (dev)</Link>
+                    <Link className="m-nav-btn m-nav-link" to="/flipcard">FlipCard</Link>
+                    <Link className="m-nav-btn m-nav-link" to="/card">Card</Link>
                 </div>
             </nav>
         );

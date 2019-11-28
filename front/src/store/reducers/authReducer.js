39eee4f005fa63ab3e6a59932d82baff56966f01
dzazef <dzazef@gmail.com>
2019-11-28 20:19:51 +0100

    small change

--- front/src/store/reducers/authReducer.js
+++ front/src/store/reducers/authReducer.js
@@ -2,6 +2,8 @@ const defaultState = {
     refreshToken: null,
     accessToken: null,
     loginErrorMessage: null,
+    registerErrorMessage: null,
+    registerSuccessful: null,
 };
 
 export const auth = (state = defaultState, action) => {
@@ -12,27 +14,41 @@ export const auth = (state = defaultState, action) => {
                 refreshToken: action.refreshToken,
                 accessToken: action.accessToken,
                 loginErrorMessage: null,
+                registerErrorMessage: null,
+                registerSuccessful: null,
             };
         case 'LOGIN_ERROR':
             return {
                 refreshToken: null,
                 accessToken: null,
                 loginErrorMessage: action.errorContent,
+                registerErrorMessage: null,
+                registerSuccessful: null,
             };
-        case 'RESET_ERROR_MESSAGE':
+        case 'RESET_STATE':
             return {
                 refreshToken: state.refreshToken,
                 accessToken: state.accessToken,
                 loginErrorMessage: null,
                 registerErrorMessage: null,
+                registerSuccessful: null,
             };
         case 'REGISTER_ERROR':
             return {
+                refreshToken: state.refreshToken,
+                accessToken: state.accessToken,
+                loginErrorMessage: null,
                 registerErrorMessage: action.errorMessage,
+                registerSuccessful: null,
             };
         case 'REGISTER_SUCCESSFUL':
-            //TO DO
-            return state;
+            return {
+                refreshToken: state.refreshToken,
+                accessToken: state.accessToken,
+                loginErrorMessage: null,
+                registerErrorMessage: null,
+                registerSuccessful: true,
+            };
         default:
             return state;
     }

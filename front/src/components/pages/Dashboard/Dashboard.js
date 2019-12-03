a65bf805d81913d69e18084193d5b8cdd7aa682e
4kuma <marek9078@gmail.com>
2019-12-03 21:12:26 +0100

    fix for Kamil

--- front/src/components/pages/Dashboard/Dashboard.js
+++ front/src/components/pages/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import NavBarUser from "../../common/navbar/NavBarUser";
 import './dashboard.css'
 import SetCard from "../../common/setCard/SetCard";
+import SetsView from "../SetsView/SetsView";
 /**
  * Komponent zawierający profil użytkownika
  */
@@ -24,31 +25,12 @@ class Dashboard extends Component {
     }
 
     render() {
-        // const {userSets} = this.state;
         return (
             <div  id="main-div">
                 <div id="navDiv">
                     <NavBarUser></NavBarUser>
                 </div>
-                <div  id="dashDiv">
-                    <div className="bg-light" id="createSet">
-
-                        <form onSubmit={(e) => {this.addState(e)}}>
-                            <p><input ref={(input) => {this.newSet = input}} type="text" /></p>
-                            <p><button>AddState</button></p>
-                        </form>
-                    </div>
-                    <div className="bg-light container" id="reviewSets">
-                        <h5 className="text-uppercase text-muted mb-0 card-title">List of sets: </h5>
-                        <div className="row top-buffer">
-                            {this.state.sets.map(item => (
-                                <div className="col-sm-6 col-lg-4"> <SetCard height={"100px"} title={item} key={item}></SetCard>
-                                <div className="top-buffer"></div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
+                <SetsView></SetsView>
             </div>
         );
     }

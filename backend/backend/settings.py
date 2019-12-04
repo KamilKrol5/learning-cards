9a922021872943887ec9dca7df4b74b1c5322c0f
Kamil <kamil55525@gmail.com>
2019-12-04 20:23:38 +0100

    Change permission classes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,7 +113,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',

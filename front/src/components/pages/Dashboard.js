94aa4e5d86daa8150413f6cd965cd9a8da6b93f8
dzazef <dzazef@gmail.com>
2019-12-04 01:21:19 +0100

    Dashboard redux

--- front/src/components/pages/Dashboard.js
+++ front/src/components/pages/Dashboard.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+import * as DashboardActions from '../../store/actions/dashboardActions'
+import {Link} from "react-router-dom";
 
 /**
  * Komponent zawierający profil użytkownika
@@ -7,9 +10,50 @@ class Dashboard extends Component {
     render() {
         return (
             <div>
+                <Link
+                    to={"#"}
+                    style={{
+                        'border-radius': '1px solid red'
+                    }}
+                    onClick={
+                        () => {
+                            this.props.setDashboardError('cipa occured')
+                        }
+                    }
+                >
+                    Click
+                </Link>
             </div>
         );
     }
 }
 
-export default Dashboard;
+const mapStateToProps = state => {
+    return {
+        dashboard: state.dashboard
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return ({
+        //DEFAULT, can be used to reset
+        setDashboardUserSetView: () => {
+            dispatch(DashboardActions.setDashboardUserSetView())
+        },
+        setDashboardPreferencesView: () => {
+            dispatch(DashboardActions.setDashboardPreferencesView())
+        },
+        setDashboardSetView: (setID) => {
+            dispatch(DashboardActions.setDashboardSetView(setID))
+        },
+        setDashboardError: (errorMessage) => {
+            dispatch(DashboardActions.setDashboardError(errorMessage))
+        },
+        resetDashboardError: () => {
+            dispatch(DashboardActions.setDashboardError(null))
+        },
+    })
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
---
+++ front/src/store/reducers/dashboardReducer.js
@@ -0,0 +1,40 @@
+const defaultState = {
+    currentView: 'DASHBOARD_USER_SET_VIEW',
+    setID: null,
+    errorMessage: null,
+};
+
+export const dashboard = (state = defaultState, action) => {
+    switch (action.type) {
+        case 'DASHBOARD_USER_SET_VIEW':
+            return {
+                currentView: action.type,
+                setID: null,
+                errorMessage: null,
+            };
+        case 'DASHBOARD_PREFERENCES_VIEW':
+            return {
+                currentView: action.type,
+                setID: null,
+                errorMessage: null,
+            };
+        case 'DASHBOARD_SET_VIEW':
+            return {
+                currentView: action.type,
+                setID: action.setID,
+                errorMessage: null,
+            };
+        case 'DASHBOARD_ERROR':
+            return {
+                currentView: state.currentView,
+                setID: state.setID,
+                errorMessage: action.errorMessage,
+            };
+        default:
+            return {
+                currentView: 'DASHBOARD_USER_SET_VIEW',
+                setID: null,
+                errorMessage: null,
+            };
+    }
+};

324ab1e8285243dae01103ce524e8bfbf5e0d621
Kamil <kamil55525@gmail.com>
2019-12-05 19:57:46 +0100

    Add changing state in the dashboard according to actions in user sets view

--- front/src/components/common/setCard/SetCard.js
+++ front/src/components/common/setCard/SetCard.js
@@ -6,7 +6,7 @@ class SetCard extends Component {
         return (
             <div className="col-sm-6 mb-4">
                 <div className="h-100 card bg-light" onClick={this.props.action}>
-                    <a href="#" className="card-link text-dark">
+                    <a href="#" className="card-link text-dark " onClick={this.props.onCardClicked}>
                         <div className=" card-body">
                             <a className="float-right btn-sm btn-light" onClick={this.props.editAction}>Edit</a>
                             <h5 className="card-title">{this.props.title}</h5>

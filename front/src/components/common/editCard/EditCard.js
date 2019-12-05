5101d5e77ce853e371479776b468c58f4fafc12d
4kuma <marek9078@gmail.com>
2019-12-05 11:40:28 +0100

    second update

--- front/src/components/common/editCard/EditCard.js
+++ front/src/components/common/editCard/EditCard.js
@@ -5,17 +5,21 @@ class EditCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            term:this.props.term,
-            definition:this.props.definition
-        }
+            term: this.props.term,
+            definition: this.props.definition
+        };
+        // this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange = (e) => {
-        e.preventDefault();
-            this.setState({
-                [e.target.name]:e.target.value
-            })
+        // e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+        this.props.onItemChange(this.state);
     };
+
+
     render() {
         return (
             <div className="m-card"
@@ -27,11 +31,13 @@ class EditCard extends Component {
             >
                 <div className="w-100 h-100 d-block">
                     <form className="container m-form">
-                        <input onChange={this.handleChange} className="col-sm-4 m-input offset-1" type="text" name="term" placeholder="Term" value={this.props.term} />
-                        <input onChange={this.handleChange} className="col-sm-4 m-input offset-2" type="text" name="definition" placeholder="Definition" value={this.props.definition} />
+                        <input onChange={this.handleChange} className="col-sm-4 m-input offset-1" type="text"
+                               name="term" placeholder="Term" defaultValue={this.props.term}/>
+                        <input onChange={this.handleChange} className="col-sm-4 m-input offset-2" type="text"
+                               name="definition" placeholder="Definition" defaultValue={this.props.definition}/>
                     </form>
                 </div>
-                <button type="button" className="close m-button" aria-label="Close">
+                <button type="button" className="close m-button" aria-label="Close" onClick={() => this.props.onDelete(this.state)}>
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>

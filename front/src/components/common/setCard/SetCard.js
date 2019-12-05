85671d00172ede3d2f636733001b9e7f1d757c7c
Kamil <kamil55525@gmail.com>
2019-12-05 20:46:16 +0100

    Update setCard style

--- front/src/components/common/setCard/SetCard.js
+++ front/src/components/common/setCard/SetCard.js
@@ -6,12 +6,12 @@ class SetCard extends Component {
         return (
             <div className="col-sm-6 mb-4">
                 <div className="h-100 card bg-light" onClick={this.props.action}>
-                    <a href="#" className="card-link text-dark " onClick={this.props.onCardClicked}>
+                    <div href="#" className="card-link text-dark clickable" onClick={this.props.onCardClicked}>
                         <div className=" card-body">
-                            <a className="float-right btn-sm btn-light" onClick={this.props.editAction}>Edit</a>
+                            <div className="float-right btn-sm btn-light clickable" onClick={this.props.editAction}>Edit</div>
                             <h5 className="card-title">{this.props.title}</h5>
                         </div>
-                    </a>
+                    </div>
                 </div>
             </div>
         );

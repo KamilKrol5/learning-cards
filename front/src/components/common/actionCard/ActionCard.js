a1e5b89cd94960106d95d998279bcf01d842c6c2
Kamil <kamil55525@gmail.com>
2021-06-13 23:46:26 +0200

    Add tests for ActionCard

--- front/src/components/common/actionCard/ActionCard.js
+++ front/src/components/common/actionCard/ActionCard.js
@@ -5,7 +5,7 @@ class ActionCard extends Component {
     render() {
         return (
             <div className="col-sm-6 mb-4">
-                <div className="h-100 card bg-success" onClick={this.props.action}>
+                <div id="card" className="h-100 card bg-success" onClick={this.props.action}>
                     <a href="#" className="card-link text-dark">
                         <div className=" card-body">
                             <h4 className="card-title">{this.props.title}</h4>
---
+++ front/src/components/common/actionCard/ActionCard.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ActionCard from './ActionCard';
+import {fireEvent, render} from "@testing-library/react";
+
+
+const dummyAction = () => {
+}
+
+test('Test properties setting', () => {
+    const component = renderer.create(
+        <ActionCard title="title" action={dummyAction}/>,
+    );
+
+    expect(component.root.props.title).toBe("title")
+    expect(component.root.props.action).toBe(dummyAction)
+});
+
+
+test('Test properties setting', () => {
+    let x = false
+    const dummyAction = () => {
+        x = true
+    }
+    const dom = render(
+        <ActionCard title="title" action={dummyAction}/>,
+    );
+
+    const card = document.getElementById('card');
+    fireEvent.click(card);
+    expect(x).toBeTruthy()
+});
